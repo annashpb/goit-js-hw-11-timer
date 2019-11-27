12c9359bc09a367beb128e22782b18485e3b83b8
Anna Sh <anna.sh.pb@gmail.com>
2019-11-27 19:03:35 +0200

    mod11timer

--- js/index.js
+++ js/index.js
@@ -1,13 +1,46 @@
 "use strict";
 
+const refs = {
+  days: document.querySelector('span[data-value="days"]'),
+  hours: document.querySelector('span[data-value="hours"]'),
+  mins: document.querySelector('span[data-value="mins"]'),
+  secs: document.querySelector('span[data-value="secs"]')
+};
 
+class CountdownTimer {
+  constructor(selector, targetDate) {
+    this.selector = selector;
+    this.targetDate = targetDate;
+  }
+  setTimer() {
+    this.timerId = setInterval(() => {
+      const currentTime = Date.now();
+      const deltaTime = this.targetDate - currentTime;
+      updateClockface(deltaTime);
+    }, 1000);
+  }
+}
 
-new CountdownTimer({
-  selector: "#timer-1",
-  targetDate: new Date("Jul 17, 2019")
-});
+const timer = new CountdownTimer(
+  "#timer-1",
+  new Date("December 17, 2020 12:00:00")
+);
 
-const days = Math.floor(time / (1000 * 60 * 60 * 24));
-const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-const secs = Math.floor((time % (1000 * 60)) / 1000);
+timer.setTimer();
+
+function pad(value) {
+  return String(value).padStart(2, "0");
+}
+
+function updateClockface(time) {
+  const days = Math.floor(time / (1000 * 60 * 60 * 24));
+  const hours = pad(
+    Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+  );
+  const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+  const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+  refs.days.textContent = days;
+  refs.hours.textContent = hours;
+  refs.mins.textContent = mins;
+  refs.secs.textContent = secs;
+}
